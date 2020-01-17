580969c6d1e720878d4de6444390ddd8337b36e3
jiziye <jiziye2016@163.com>
2020-01-17 15:23:02 +0800

    L4

---
+++ L4/StereoDisparity/buildc/CMakeFiles/StereoDisparity.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StereoDisparity.dir/disparity.cpp.o"
+  "StereoDisparity.pdb"
+  "StereoDisparity"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StereoDisparity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
